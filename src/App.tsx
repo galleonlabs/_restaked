0ff187cc33029e9c4d086b2498b7be0d25ccbc70
Davy Jones <hello@galleon.community>
2023-10-31 16:00:43 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ function App() {
               Become a chad,
              
             </h2>
-            <a href='https://app.eigenlayer.xyz/?utm_source=eigenchad' className='uppercase rounded-md bg-[#1a0c6d] px-3.5 py-2.5 text-md font-semibold text-white shadow-sm hover:text-white hover:bg-[#0e0544] focus-visible:outline border border-[#1a0c6d]  focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#0e0544]'>Restake Now
+            <a href='https://app.eigenlayer.xyz/?utm_source=eigenchad' target='_blank' className='uppercase rounded-md bg-[#1a0c6d] px-3.5 py-2.5 text-md font-semibold text-white shadow-sm hover:text-white hover:bg-[#0e0544] focus-visible:outline border border-[#1a0c6d]  focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#0e0544]'>Restake Now
             
               <img
                 className="inline-block h-6 w-6 mb-1.5 ml-2 rounded-full"
@@ -36,7 +36,7 @@ function App() {
                 alt=""
               />
             </a>
-            <a href="https://docs.eigenlayer.xyz/overview/readme&utm_source=eigenchad" target='_blank' className="ml-4 uppercase rounded-md bg-white px-3.5 py-2.5 text-md font-semibold text-[#1a0c6d] shadow-sm hover:text-[#1a0c6d] hover:opacity-60 border border-[#1a0c6d] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#0e0544]">
+            <a href="https://docs.eigenlayer.xyz/?utm_source=eigenchad" target='_blank' className="ml-4 uppercase rounded-md bg-white px-3.5 py-2.5 text-md font-semibold text-[#1a0c6d] shadow-sm hover:text-[#1a0c6d] hover:opacity-60 border border-[#1a0c6d] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#0e0544]">
               Learn more<span aria-hidden="true"> →</span>
             </a>
     
