8a4ead2030c215f19ca93152750e206291b9df02
Davy Jones <hello@galleon.community>
2023-10-31 16:28:00 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -36,8 +36,8 @@ function App() {
                 alt=""
               />
             </a>
-            <a href="https://docs.eigenlayer.xyz/?utm_source=eigenchad" target='_blank' className="ml-4 uppercase rounded-md bg-white px-3.5 py-2.5 text-md font-semibold text-[#1a0c6d] shadow-sm hover:text-[#1a0c6d] hover:opacity-60 border border-[#1a0c6d] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#0e0544]">
-              Learn more<span aria-hidden="true"> →</span>
+            <a href="https://docs.eigenlayer.xyz/?utm_source=eigenchad" target='_blank' className="mt-2 sm:mt-0 border-0 sm:border block sm:inline-flex ml-4 uppercase rounded-md bg-white px-3.5 py-2.5 text-md font-semibold text-[#1a0c6d] shadow-sm hover:text-[#1a0c6d] hover:opacity-60 border-[#1a0c6d] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#0e0544]">
+              Learn more <span aria-hidden="true">&nbsp;→</span>
             </a>
     
           </div>
