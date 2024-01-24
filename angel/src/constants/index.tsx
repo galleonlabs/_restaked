a700f419dab2d95d2a4808ef21c300b0b5f16a50
Davy Jones <hello@galleon.community>
2024-01-24 17:49:07 +0000

    Update index.tsx

--- angel/src/constants/index.tsx
+++ angel/src/constants/index.tsx
@@ -4,15 +4,7 @@ const projects: any = [
 
 const team = [
   {
-    name: "EigenIntern",
-    url: "https://twitter.com/eigenintern",
-    imageSrc: "https://pbs.twimg.com/profile_images/1717240573877256192/HNEU1QuK_400x400.jpg",
-    imagePadding: '',
-    imageAlt: "Profile picture of EigenIntern",
-    handle: "@eigenintern"
-  },
-  {
-    name: "EigenChad",
+    name: "EigenChad - _Restaked Markets & Investments",
     url: "https://twitter.com/eigenchad",
     imageSrc: "https://pbs.twimg.com/profile_images/1715776754319491073/lWtwXpRt_400x400.jpg",
     imagePadding: '',
@@ -20,12 +12,20 @@ const team = [
     handle: "@eigenchad"
   },
   {
-    name: "_Restaked",
+    name: "EigenIntern - _Restaked Founder",
+    url: "https://twitter.com/eigenintern",
+    imageSrc: "https://pbs.twimg.com/profile_images/1717240573877256192/HNEU1QuK_400x400.jpg",
+    imagePadding: '',
+    imageAlt: "Profile picture of EigenIntern",
+    handle: "@eigenintern"
+  },
+  {
+    name: "_Restaked - Community",
     url: "https://t.co/R3RO2lqQ6H",
     imageSrc: "https://pbs.twimg.com/profile_images/1719768085815803905/Qt-WhTGg_400x400.jpg",
     imagePadding: '',
     imageAlt: "Profile picture of Discord",
-    handle: "Ethereum community, contributors & angels."
+    handle: "Crypto natives, contributors & angels."
   },
 ];
 
